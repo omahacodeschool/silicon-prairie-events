3451ab44618c3f8bc828458d4164d6423a1afc1b
Matt Taylor <Matt@Navgear.com>
2014-10-17 11:40:41 -0500

    Methods for each route

--- lib/silicon_prairie_event.rb
+++ lib/silicon_prairie_event.rb
@@ -31,5 +31,55 @@ module SiliconPrairieEvent
       end
       all_events
     end
+    
+    def self.lincoln_events
+      lincoln_events = []
+      response = HTTParty.get("http://event-api.herokuapp.com/api/v1/startup_lincoln_events")
+      
+      response.each do |hash|
+        lincoln_events << Event.new(hash)
+      end
+      lincoln_events
+    end
+    
+    def self.events_all_week
+      week_events = []
+      response = HTTParty.get("http://event-api.herokuapp.com/api/v1/events_all_week")
+      
+      response.each do |hash|
+        week_events << Event.new(hash)
+      end
+      week_events
+    end
+    
+    def self.events_by_month(month)
+      this_month = []
+      response = HTTParty.get("http://event-api.herokuapp.com/api/v1/events_by_month/#{month}")
+      
+      response.each do |hash|
+        this_month << Event.new(hash)
+      end
+      this_month
+    end
+    
+    def self.events_near(range)
+      near_events = []
+      response = HTTParty.get("http://event-api.herokuapp.com/api/v1/events_near/#{range}")
+      
+      response.each do |hash|
+        near_events << Event.new(hash)
+      end
+      near_events
+    end
+    
+    def self.rest_of_week
+      rest_of_week = []
+      response = HTTParty.get("http://event-api.herokuapp.com/api/v1/events_rest_of_week")
+      
+      response.each do |hash|
+        rest_of_week << Event.new(hash)
+      end
+      rest_of_week
+    end
   end
 end
